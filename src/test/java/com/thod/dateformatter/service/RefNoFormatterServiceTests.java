c11e075e21937376ca98903f061ad74d8a718e98
oattsp <thodsaphon.aiab@gmail.com>
2022-07-03 14:00:25 +0700

    optimization unit test

--- src/test/java/com/thod/dateformatter/service/RefNoFormatterServiceTests.java
+++ src/test/java/com/thod/dateformatter/service/RefNoFormatterServiceTests.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 @SpringBootTest
 public class RefNoFormatterServiceTests {
 
